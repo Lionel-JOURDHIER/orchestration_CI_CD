eb139a5f94a40fe6d7282902e9d1920699c083ae
Lionel JOURDHIER <lio.jourdhier@gmail.com>
2026-03-06 13:21:29 +0100

    Mise à jour de conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,35 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Orchestration'
-copyright = '2026, Lionel-Jourdhier'
-author = 'Lionel-Jourdhier'
-release = '2026'
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../"))
+
+project = "MLObs2"
+copyright = "2026, Lionel JOURDHIER"
+author = "Lionel JOURDHIER"
+release = "2026"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",  # INDISPENSABLE : extrait la doc des docstrings
+    "sphinx.ext.napoleon",  # Supporte le format Google/NumPy (plus lisible)
+    "sphinx.ext.viewcode",  # Ajoute un lien [source] à côté de tes fonctions
+    "sphinx.ext.mathjax",  # Pour le rendu des formules LaTeX
+    "myst_parser",  # Pour lire les fichiers .md (README, etc.)
+    "sphinxcontrib.bibtex",  # Pour la gestion du fichier .bib
+]
+bibtex_bibfiles = ["refs.bib"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
